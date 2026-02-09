c5df3d055d985acf7a04d6ba440c7bf1ff4bb356
AnimeshJ004 <animeshj425@gmail.com>
2026-02-09 19:41:45 +0530

    Fix login problrm

--- src/main/java/com/RiskAnalyzerProject/ContractRiskAnalyzer/repository/UserRepository.java
+++ src/main/java/com/RiskAnalyzerProject/ContractRiskAnalyzer/repository/UserRepository.java
@@ -2,15 +2,17 @@ package com.RiskAnalyzerProject.ContractRiskAnalyzer.repository;
 
 import com.RiskAnalyzerProject.ContractRiskAnalyzer.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+    @Query("{ 'username' : { $regex: ?0, $options: 'i' } }")
     Optional<User> findByUsername(String username);
+    @Query(value = "{ 'username' : { $regex: ?0, $options: 'i' } }", exists = true)
     Boolean existsByEmail(String email);
-
+    @Query("{ 'email' : { $regex: ?0, $options: 'i' } }")
     Optional<User> findByEmail(String email);
-
+    @Query(value = "{ 'email' : { $regex: ?0, $options: 'i' } }", exists = true)
     boolean existsByUsername(String username);
 }
